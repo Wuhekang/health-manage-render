7b9fbe004b70531ea85d2c0074906d9386b83c0c
康康康 <18753817379@163.com>
2026-06-06 22:12:57 +0800

    Fix MySQL startup wait on Render

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,13 +2,8 @@
 set -eu
 
 docker-entrypoint.sh mysqld --bind-address=127.0.0.1 &
-db_pid=$!
 
 until mysqladmin ping --host=127.0.0.1 --user=root --password="$MYSQL_ROOT_PASSWORD" --silent; do
-  if ! kill -0 "$db_pid" 2>/dev/null; then
-    echo "MySQL exited before becoming ready"
-    exit 1
-  fi
   sleep 1
 done
 
